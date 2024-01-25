edc61788511cd1c37c955fb300bd2895cd71835d
Ash2700 <vmvuruvu@gmail.com>
2024-01-25 22:46:19 +0800

    templement register

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ router.get('/',(req, res, next)=>{
 router.get('/login',(req, res, next)=>{
     res.render('login')
 })
+
 router.get('/register', (req, res, next)=>{
     res.render('register')
 })
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,42 @@
-const express= require('express')
+const express = require('express')
 const router = express.Router()
+const db = require('../models')
+const Users = db.Users
+const bcrypt = require('bcryptjs')
+
+router.post('/register', async (req, res, next) => {
+
+    const { name, email, password, confirmPassword } = req.body
+
+
+    if (!email || !password || !name) {
+
+        return res.redirect('back')
+    }
+    if (password !== confirmPassword) {
+
+        return res.redirect('back')
+    }
+    const isReapCount = await Users.count({ where: { email } })
+    await console.log(isReapCount)
+
+    if (isReapCount > 0) {
+
+        return res.redirect('back')
+    }
+    const hash = await bcrypt.hash(password, 10)
+    return Users.create({ name, email, password: hash })
+        .then(() => {
+
+            res.redirect('/login')
+        })
+        .catch((error) => {
+            next(error)
+        })
+})
 
 
 
 
 
-
-
-module.exports= router
+module.exports = router
