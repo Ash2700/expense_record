f39bba2f59bb6f88292d0fda6f5e951063473da6
Ash2700 <vmvuruvu@gmail.com>
2024-01-25 19:16:29 +0800

    login router build

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express =require('express')
 const { engine } = require('express-handlebars')
 const app = express()
 const router= require('./routes')
-
+const port= 3000
 
 
 
@@ -11,3 +11,9 @@ app.set('view engine','.hbs')
 app.set('views','./views')
 
 app.use(router)
+
+
+
+app.listen(port,(()=>{
+    console.log(`express server is running on http://localhost:${port}`)
+}))
--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,23 @@ const router = express.Router()
 const record= require('./record')
 
 
+router.use('/record',record)
+
+router.get('/',(req, res, next)=>{
+    res.redirect('/login')
+})
+
+
+router.get('/login',(req, res, next)=>{
+    res.send('login')
+})
+
+router.get('/register', (req, res, next)=>{
+    res.send('register')
+})
+
+router.post('/logout', (req, res, next)=>{
+    res.send('logout')
+})
+
 module.exports=router
