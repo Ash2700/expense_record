016d94a4f7044d9d6148308d38be82fe610ccb73
Ash2700 <vmvuruvu@gmail.com>
2024-01-25 20:20:24 +0800

    build migration table User & seeder Users

---
+++ seeders/20240125115747-userSeeder.js
@@ -0,0 +1,32 @@
+'use strict';
+const bcrypt= require('bcryptjs');
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const hash =await bcrypt.hash('123456',10)
+   await queryInterface.bulkInsert('Users',[{
+
+    name:'John',
+    email:'father@mail.com',
+    password:hash,
+    createdAt:new Date(),
+    updatedAt:new Date()
+   },{
+    name:'Sally',
+    email:'mother@mail.com',
+    password:hash,
+    createdAt:new Date(),
+    updatedAt:new Date()
+   }],{})
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Users',null,{})
+  }
+};
