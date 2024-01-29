0ac1834c0bbc26d7a5b8b224ae3e880de74f12d2
Ash2700 <vmvuruvu@gmail.com>
2024-01-29 11:22:54 +0800

    templement edit page &router

--- routes/record.js
+++ routes/record.js
@@ -8,21 +8,20 @@ const zeroString = '0'
 // 修改資料
 function dataModify (rawDataFormDatabase) {
   return rawDataFormDatabase.map((item) => {
-    item.date = dateFormChange(item)
-    item.label = giveLabelMatchID(item)
+    dateFormChange(item)
+    giveLabelMatchID(item)
     return item
   })
 }
 // 修改時間格式
 function dateFormChange (data) {
-  return new Date(data.date).toLocaleDateString('zh-TW', { year: 'numeric', month: '2-digit', day: '2-digit' })
+  return data.date=new Date(data.date).toISOString().split('T')[0]
 }
-
 // 加入圖示標籤到資料裡
 function giveLabelMatchID (item) {
   const matchGroup = categoryData.find((group) => (group.id === item.categoryID))
   if (matchGroup) {
-    return matchGroup.label
+    return item.label =matchGroup.label
   } else {
     return null
   }
@@ -42,7 +41,7 @@ router.get('/', async (req, res, next) => {
   await dataModify(recordData)
   return res.render('home', { records: recordData, totalAmount, selected: categoryID })
 })
-
+//get sum form category or all
 async function sumFromCategory (categoryIDFormReq, userId) {
   const id = categoryIDFormReq || zeroString
   if (id === zeroString) {
@@ -61,20 +60,18 @@ async function sumFromCategory (categoryIDFormReq, userId) {
 }
 
 router.get('/add', (req, res, next) => {
-  
-  
   res.render('expense',{categoryData})
 })
 
 router.post('/add',  (req, res, next) => {
   const {name, date, categoryId , amount} = req.body
-  const userID=req.user.id
+  const usingID=req.user.id
   const checkInfo= checkData(req.body)
   if(checkInfo.result===false){
     req.flash('error',`${checkInfo.message}`)
     return res.redirect('back')
   }
-  return record.create({name,date,categoryID:categoryId,amount,userID})
+  return record.create({name,date,categoryID:categoryId,amount,userID:usingID})
     .then(()=>{
       req.flash('success','新增成功')
       res.redirect('/records')
@@ -85,8 +82,8 @@ router.post('/add',  (req, res, next) => {
     })
 })
 //檢查資料
-function checkData(dataGroup){
-  const {name, date, categoryId, amount} =dataGroup
+function checkData(reqBodyData){
+  const {name, date, categoryId, amount} =reqBodyData
   let info={result:false,message:''}
   if(!name || !date || !categoryId || !amount){
     info.message='必填欄位需輸入'
@@ -96,10 +93,37 @@ function checkData(dataGroup){
   return info
 }
 
-router.get('/edit', (req, res, next) => {
-  res.send('edit expense')
+router.get('/edit/:id', async (req, res, next) => {
+  const recordID= req.params.id
+  
+  const usingID= req.user.id
+  const recordData =await record.findOne({
+    attributes:['id', 'name' , 'date', 'amount', 'userID', 'categoryID'],
+    where:{id:recordID,userID:usingID},
+    raw:true
+  })
+  if(!recordData){
+    req.flash('error','查無此資料')
+    return res.redirect('back')
+  }
+  if(recordData.userID !== usingID){
+    req.flash('error','權限不足')
+    return res.redirect('back')
+  }
+  dateFormChange(recordData)
+  return res.render('editRecord',{categoryData,record:recordData})
 })
-router.put('/', (req, res, next) => {
+
+router.put('/edit/:id', (req, res, next) => {
+  const {name , date, amount, categoryId}=req.body
+  const recordId = req.params.id
+  const usingID= req.user.id
+  return record.update({
+  name,date,amount,categoryID:categoryId},{where:{id:recordId,userID:usingID}})
+  .then(()=>{
+  req.flash('success','更新成功')
+  
+  })
   res.send('record put')
 })
 
